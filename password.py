c27a9e5dc1b7c610e23fda2a33f774a7c203bb23
takeryang <takeryang@gmail.com>
2019-07-01 22:13:19 +0800

    No1 file

---
+++ password.py
@@ -0,0 +1,15 @@
+print('這是一個猜密碼的遊戲程式')
+
+password = 'a123456'
+i = 3
+while True:
+	psw = input('請輸入密碼: ')
+	if psw == password:
+		print('登入成功!')
+		break
+	else:
+		i = i - 1
+		print('密碼錯誤! 還有' , i, '次機會')
+		if i == 0:
+			print('遊戲失敗!')
+			break
